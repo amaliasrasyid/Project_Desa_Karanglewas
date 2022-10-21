2c606ece4dc4eb6d17b378498743dcf7796ddfce
AgusJems <safrudinagus071@gmail.com>
2022-10-21 17:41:39 +0700

    Stysla Template

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/website_desa', function () {
     return view('layouts.master');
+});
+Route::get('/', function () {
+    return view('index');
 });
