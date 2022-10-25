e1c3cc69ca7ede13dd0b09985a0293b2262bec58
AgusJems <safrudinagus071@gmail.com>
2022-10-25 22:05:00 +0700

    Tambah Menu

--- resources/views/layouts/master-1.blade.php
+++ resources/views/layouts/master-1.blade.php
@@ -242,7 +242,7 @@
       <div class="main-sidebar sidebar-style-2">
         <aside id="sidebar-wrapper">
           <div class="sidebar-brand">
-            <a href="index.html">Stisla</a>
+            <a href="index.html">Website Desa</a>
           </div>
           <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">St</a>
