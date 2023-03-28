19e5c11be555fea61a5bd415a736df01220950a1
AgusJems <59831192+AgusJems@users.noreply.github.com>
2023-03-28 10:33:51 +0700

    yuhu

--- app/Http/Controllers/PkhController.php
+++ app/Http/Controllers/PkhController.php
@@ -14,4 +14,15 @@ class PKHController extends Controller
         $pkh = Penduduk::latest()->paginate(10);
         return view('pkh.index', compact('pkh'))->with('i', (request()->input('page', 1) - 1) * 10);
     }
+
+    public function create()
+    {
+        return view('pkh.create');
+    }
+
+    public function edit()
+    {
+        return view('pkh.edit');
+    }
+
 }
